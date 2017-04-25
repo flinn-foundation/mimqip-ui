f184195f6777b8816a427a25ca24e9f8d366aa3d
Michael Iden <michaeliden90@gmail.com>
2017-04-25 12:03:04 -0400

    Completed mental status and psych evaluation pages

--- src/app/pages/patient-details-page/tabs/evaluations/mental-status/mental-status.component.ts
+++ src/app/pages/patient-details-page/tabs/evaluations/mental-status/mental-status.component.ts
@@ -1,13 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import {EvaluationDto} from "../../../../../swagger-patient-service/model/EvaluationDto";
+import EvaluationTypeEnum = EvaluationDto.EvaluationTypeEnum;
+import {EvaluationBase} from "../evaluation-base";
+import {EvaluationService} from "../../../../../services/evaluation/evaluation.service";
 
 @Component({
   selector: 'app-mental-status',
   templateUrl: './mental-status.component.html',
   styleUrls: ['./mental-status.component.scss']
 })
-export class MentalStatusComponent implements OnInit {
+export class MentalStatusComponent extends EvaluationBase implements OnInit {
 
-  constructor() { }
+  evaluationDto: EvaluationDto = {
+    evaluationType: EvaluationTypeEnum.MENTALSTATUS,
+    evaluationResponses: [
+      {prompt: "Appearance and General Behavior", answer: ""},
+      {prompt: "Motor Activity", answer: ""},
+      {prompt: "Speech", answer: ""},
+      {prompt: "Mood and Affect", answer: ""},
+      {prompt: "Thought Process", answer: ""},
+      {prompt: "Thought Content", answer: ""},
+      {prompt: "Perceptual Disturbances", answer: ""},
+      {prompt: "Sensorium and Cognition", answer: ""},
+      {prompt: "Insight", answer: ""},
+      {prompt: "Judgement", answer: ""}
+    ]
+  };
+
+  constructor(evaluationService: EvaluationService) {
+    super(evaluationService);
+  }
 
   ngOnInit() {
   }
--- src/app/pages/patient-details-page/tabs/evaluations/psychiatric/psychiatric.component.ts
+++ src/app/pages/patient-details-page/tabs/evaluations/psychiatric/psychiatric.component.ts
@@ -1,13 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import {EvaluationBase} from "../evaluation-base";
+import {EvaluationService} from "../../../../../services/evaluation/evaluation.service";
+import {EvaluationDto} from "../../../../../swagger-patient-service/model/EvaluationDto";
+import EvaluationTypeEnum = EvaluationDto.EvaluationTypeEnum;
 
 @Component({
   selector: 'app-psychiatric',
   templateUrl: './psychiatric.component.html',
   styleUrls: ['./psychiatric.component.scss']
 })
-export class PsychiatricComponent implements OnInit {
+export class PsychiatricComponent extends EvaluationBase implements OnInit {
 
-  constructor() { }
+  evaluationDto: EvaluationDto = {
+    evaluationType: EvaluationTypeEnum.PSYCHIATRIC,
+    evaluationResponses: [
+      {prompt: "Reasons for Evaluation", answer: ""},
+      {prompt: "History of Present Illness", answer: ""},
+      {prompt: "Past Psychiatric History", answer: ""},
+      {prompt: "History of Alcohol and Other Substance Abuse", answer: ""},
+      {prompt: "General Medical History", answer: ""},
+      {prompt: "Developmental, Psychosocial and Sociocultural History", answer: ""},
+      {prompt: "Occupational and Military History", answer: ""},
+      {prompt: "Legal History", answer: ""},
+      {prompt: "Family History", answer: ""},
+      {prompt: "Review of Systems", answer: ""},
+      {prompt: "Functional Assessments", answer: ""}
+    ]
+  };
+
+  constructor(evaluationService: EvaluationService) {
+    super(evaluationService);
+  }
 
   ngOnInit() {
   }
